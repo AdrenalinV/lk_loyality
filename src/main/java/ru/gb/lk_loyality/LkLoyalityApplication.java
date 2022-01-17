95b5f3529b38c10ad33245834400f030a552a9a6
Adrenalin <vadimtsh@gmail.com>
2022-01-17 21:11:45 +0300

    create project

---
+++ src/main/java/ru/gb/lk_loyality/LkLoyalityApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.lk_loyality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkLoyalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkLoyalityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/lk_loyality/LkLoyalityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.lk_loyality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkLoyalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
